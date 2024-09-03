b0c2249b7cbc4c727d189f9bf45d5ea93ffa8fd5
cruix89 <rafaelbuenomoraes89@gmail.com>
2024-09-03 19:44:37 -0300

    download checker develop (beta-test)

--- root/app/youtube-dl/yt-dlp-batch-check.sh
+++ root/app/youtube-dl/yt-dlp-batch-check.sh
@@ -1,47 +1,89 @@
 #!/usr/bin/with-contenv bash
 
-# Caminho do arquivo channels.txt
-channels_file="/config/channels.txt"
-# Caminho da pasta onde o channels.txt está localizado
-base_dir=$(dirname "$channels_file")
-# Arquivos de saída
-download_archive="$base_dir/download-archive.txt"
-failed_videos="$base_dir/failed-videos.txt"
+if $youtubedl_debug; then youtubedl_args_verbose=true; else youtubedl_args_verbose=false; fi
+if grep -qPe '^(--output |-o ).*\$\(' '/config/args.conf'; then youtubedl_args_output_expand=true; else youtubedl_args_output_expand=false; fi
+if grep -qPe '^(--format |-f )' '/config/args.conf'; then youtubedl_args_format=true; else youtubedl_args_format=false; fi
+if grep -qPe '^--download-archive ' '/config/args.conf'; then youtubedl_args_download_archive=true; else youtubedl_args_download_archive=false; fi
 
-# Cria os arquivos se ainda não existirem
-touch "$download_archive"
-touch "$failed_videos"
+youtubedl_binary='yt-dlp'
+exec="$youtubedl_binary"
+exec+=" --config-location '/config/args.conf'"
+exec+=" --simulate"
+exec+=" --batch-file '/tmp/urls'"; (cat '/config/channels.txt'; echo '') > '/tmp/urls.temp'
 
-# Converte arquivos de saída em listas para verificação mais rápida
-downloaded_links=$(cat "$download_archive")
-failed_links=$(cat "$failed_videos")
+if $youtubedl_args_verbose; then exec+=" --verbose"; fi
+if $youtubedl_args_output_expand; then exec+=" $(grep -Pe '^(--output |-o ).*\$\(' '/config/args.conf')"; fi
+if [ -f '/config/cookies.txt' ]; then exec+=" --cookies '/config/cookies.txt'"; fi
+if $youtubedl_subscriptions; then echo 'https://www.youtube.com/feed/channels' >> '/tmp/urls.temp'; fi
+if $youtubedl_watchlater; then echo ":ytwatchlater | --playlist-end '-1' --no-playlist-reverse" >> '/tmp/urls.temp'; fi
+if ! $youtubedl_args_format; then exec+=" --format '$(cat '/config.default/format')'"; fi
+if ! $youtubedl_args_download_archive; then exec+=" --download-archive '/config/archive.txt'"; fi
 
-# Loop através de cada URL em channels.txt
-while IFS= read -r url
+if [ -f '/config/pre-execution.sh' ]
+then
+  echo '[pre-execution] running pre-execution script...'
+  bash '/config/pre-execution.sh'
+  echo '[pre-execution] finished pre-execution script.'
+fi
+
+while [ -f '/tmp/updater-running' ]; do sleep 1s; done
+youtubedl_version="$($youtubedl_binary --version)"
+youtubedl_last_run_time="$(date '+%s')"
+echo ''; echo "$(date '+%Y-%m-%d %H:%M:%S') - starting execution"
+
+if $youtubedl_lockfile; then touch '/downloads/.youtubedl-running' && rm -f '/downloads/.youtubedl-completed'; fi
+
+while [ -f '/tmp/urls.temp' ]
 do
-  # Expande o link do canal do YouTube em todos os links de vídeo
-  yt-dlp --flat-playlist --get-id --get-title --get-url "$url" | while IFS= read -r video_url
-  do
-    # Verifica se o link já está em uma das listas
-    if grep -Fxq "$video_url" <<< "$downloaded_links"; then
-      echo "Link já está no download-archive.txt, pulando: $video_url"
-      continue
-    elif grep -Fxq "$video_url" <<< "$failed_links"; then
-      echo "Link já está no failed-videos.txt, pulando: $video_url"
-      continue
-    fi
+  extra_url_args=''
+  if grep -qPe '\|' '/tmp/urls.temp'
+  then
+    grep -m 1 -nPe '\|' '/tmp/urls.temp' > '/tmp/urls'
+    sed -i -E "$(grep -oPe '^[0-9]+' /tmp/urls)d" '/tmp/urls.temp'
+    extra_url_args="$(grep -oPe '.*?\|\K.*' '/tmp/urls')"
+    sed -i -E 's!([0-9]*:)?(.*?)(\|.*)!\2!' '/tmp/urls'
+  else
+    mv '/tmp/urls.temp' '/tmp/urls'
+  fi
 
-    # Verifica se o vídeo pode ser baixado ou se há algum bloqueio/erro
-    if yt-dlp --simulate --no-warnings --quiet "$video_url" > /dev/null 2>&1; then
-      # Se o vídeo pode ser completado, grava o link no download-archive.txt
-      echo "$video_url" >> "$download_archive"
-    else
-      # Se houver erro, grava o link no failed-videos.txt
-      echo "$video_url" >> "$failed_videos"
-    fi
-  done
-done < "$channels_file"
+  url=$(cat /tmp/urls)
+  output_path=$(dirname "$(realpath "/config/channels.txt")")
+  download_archive="$output_path/download-archive.txt"
+  failed_videos="$output_path/failed-videos.txt"
 
-echo "Verificação completa. Resultados salvos em:"
-echo " - $download_archive"
-echo " - $failed_videos"
+  if eval "$exec $extra_url_args"
+  then
+    echo "$url" >> "$download_archive"
+  else
+    echo "$url" >> "$failed_videos"
+  fi
+
+  rm -f '/tmp/urls'
+done
+
+if $youtubedl_lockfile; then touch '/downloads/.youtubedl-completed' && rm -f '/downloads/.youtubedl-running'; fi
+
+if [ $(( ($(date '+%s') - $youtubedl_last_run_time) / 60 )) -ge 2 ]
+then
+  echo ''; echo "$(date '+%Y-%m-%d %H:%M:%S') - execution took $(( ($(date '+%s') - $youtubedl_last_run_time) / 60 )) minutes"
+else
+  echo ''; echo "$(date '+%Y-%m-%d %H:%M:%S') - execution took $(( ($(date '+%s') - $youtubedl_last_run_time) )) seconds"
+fi
+
+if [ -f '/config/post-execution.sh' ]
+then
+  echo '[post-execution] running post-execution script...'
+  bash '/config/post-execution.sh'
+  echo '[post-execution] finished post-execution script.'
+fi
+
+echo "$youtubedl_binary version: $youtubedl_version"
+
+if [ "$youtubedl_interval" != 'false' ]
+then
+  echo "waiting $youtubedl_interval.."
+  sleep $youtubedl_interval
+else
+  echo "youtubedl_interval is set to 'false', container will now exit."
+  s6-svscanctl -t '/var/run/s6/services'
+fi
