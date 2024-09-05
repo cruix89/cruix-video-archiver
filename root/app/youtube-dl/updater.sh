cc757ab5725fc51c3989565e2647952e85fa9717
cruix89 <rafaelbuenomoraes89@gmail.com>
2024-09-05 07:22:44 -0300

    Release: v2024.09.05

--- root/app/youtube-dl/updater.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/with-contenv bash
-
-touch '/tmp/updater-running'; sleep 1m
-python3 -m pip --no-cache-dir --quiet install --upgrade yt-dlp > /dev/null
-rm -f '/tmp/updater-running'
-sleep 3h
