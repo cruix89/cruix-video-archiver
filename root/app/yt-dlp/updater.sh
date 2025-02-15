d5352e621870c99663577237081ad804ff064c30
Rafael Moraes <rafaelbuenomoraes89@gmail.com>
2025-02-15 11:36:10 -0300

    Release: v2025.02.27

--- root/app/yt-dlp/updater.sh
+++ root/app/yt-dlp/updater.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/with-contenv bash
 
 touch '/tmp/updater-running'; sleep 1m
-python3 -m pip --no-cache-dir --quiet install --upgrade yt-dlp > /dev/null
+pip3 --no-cache-dir install --upgrade --break-system-packages yt-dlp
 rm -f '/tmp/updater-running'
 sleep 3h
--- root/config.default/post-execution.sh
+++ root/config.default/post-execution.sh
@@ -9,14 +9,14 @@ logs_dir="/config/logs"
 # remove cache files in the output directory and process post-processing scripts
 if [ -d "$downloads_dir" ]; then
 
-  echo -e "[cruix-video-archiver] Initiating Cleanup Protocol... Purging Cache Files From the Following Directories: /cache and /logs"
+  echo -e "\e[32m[cruix-video-archiver] Initiating Cleanup Protocol... Purging Cache Files From the Following Directories: /cache and /logs\e[0m"
 
   sleep '5'
   mkdir -p $cache_dir
   find $cache_dir -type f -delete
   find $cache_dir -type d -empty -mindepth 1 -delete
 
-  echo -e "[cruix-video-archiver] Executing FFMPEG Process In The Video Library..."
+  echo -e "\e[36m[cruix-video-archiver] Executing ffmpeg Process In The Video Library...\e[0m"
 
   # post-processing scripts in downloads folder
 
@@ -29,5 +29,5 @@ if [ -d "$downloads_dir" ]; then
   /app/scripts/ffmpeg_encoder.sh
 
 else
-  echo -e "[cruix-video-archiver] Oops! Output Directory Not Found: $downloads_dir "
+  echo -e "\e[31m[cruix-video-archiver] Oops! Output Directory Not Found: $downloads_dir\e[0m"
 fi
