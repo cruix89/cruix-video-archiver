eade44af7e281cffce7b844dc8408abaae8b01e4
Rafael Moraes <rafaelbuenomoraes89@gmail.com>
2025-02-16 21:36:34 -0300

    Release: v2025.03.04

--- root/app/yt-dlp/updater.sh
+++ root/app/yt-dlp/updater.sh
@@ -5,3 +5,4 @@ touch '/tmp/updater-running'; sleep 1m
 pip3 --no-cache-dir install --upgrade --break-system-packages yt-dlp 2>&1 | grep -v "WARNING: Running pip as the 'root'"
 rm -f '/tmp/updater-running'
 echo -e "\033[1;35m[cruix-video-archiver] yt-dlp is up to date.\033[0m"
+sleep 1h
